0c1701cce2a66aede3f32debbd89ebe12414a60f
Nathan <contact@nathanhigley.com>
2020-02-28 14:31:20 -0500

    Finished indexer and indexed some notes

--- notes/indexer.py
+++ notes/indexer.py
@@ -21,7 +21,9 @@ class Indexer():
         # Set which settings are essential to set
         self.essential_settings = [
             "html-folder", 
-            "index-file"
+            "index-file",
+            "note-item-file",
+            "note-category-file"
                         ]
 
         # Set what the markers look for
@@ -45,7 +47,9 @@ class Indexer():
             self.files_to_process = self.get_files_to_index()
 
             for file in self.files_to_process:
-                process_file(self, file)
+                print(file)
+                self.process_file(file)
+                self.add_file_to_notes(file)
 
 
 
@@ -108,7 +112,8 @@ class Indexer():
             if '.html' in file:
                 existing_files.append(full_path)
 
-        for file in existing_files:
+        for x in range(len(existing_files)):
+            file = existing_files[0]
             # Store the line
             line = None
             # Open each file and grab the third line
@@ -116,7 +121,7 @@ class Indexer():
                 line = current_file.readline().strip()
                 
             # Check if the file has been indexed or if it is blank
-            if line == "" or line == "<!--*INDEXED*-->":
+            if line == "" or "<!--*INDEXED*-->" in line:
                 existing_files.remove(file)
         # Return the list of paths which need to be indexed
         return existing_files
@@ -125,98 +130,130 @@ class Indexer():
 
         # Define variables
         note_category = None
-        note_name = filename
+        note_name = filename.split('/')[1].split('.html')[0]
+        note_line = None
+        category_line = None
+
 
         # Assign category based on filename
-        if "-" in filename:
+        if "-" in filename.split('/')[1]:
             split_name = filename.split("-")
             note_category = split_name[0]
-            note_name = split_name[1]
-
-        replacements = {
-            "<!--*NOTEURL*-->": filename,
-            "<!--*NOTENAME*-->": note_name,
-            "<!--*CATEGORYNAME*-->": note_category
-        }
-
-        variables = [
-            "<!--*NOTEURL*-->",
-            "<!--*NOTENAME*-->",
-            "<!--*CATEGORYNAME*-->",
-            "<!--*STARTEDIT*-->",
-            "<!--*ENDEDIT*-->"
-        ]
-
-
-def process_file(self, filename):
-
-    # Open file and temp file to write to
-    old_file = open(filename, 'r')
-    temp_filename = str(filename+'.tmp')
-    temp_file = open(temp_filename, 'a')
-    
-    operations_to_do = self.operations
-    markers_to_find = self.markers
-    
-    temp_file.write("<!--*INDEXED*-->\n")
-
-    # Iterate through and parse the file
-    # Doing one operation at a time
-    num_line = 0
-    for line in old_file:
+            note_name = split_name[1].split('.html')[0]
+            with open(str(self.resource_folder + self.settings["note-category-file"])) as cat_file:
+                category_line = cat_file.readline()
+            category_line = category_line.replace("<!--*CATEGORYNAME*-->", note_category) + '\n'
         
-        current_line = line.strip()
-        if len(markers_to_find) > 0 and markers_to_find[operations_to_do[0]][0] in current_line:
-            # Set the current operation to the next to do
-            op = operations_to_do[0]
-            # Set the beginnning marker
-            marker_s = markers_to_find[operations_to_do[0]][0]
-            # Set the end marker
-            marker_e = markers_to_find[operations_to_do[0]][1]
-            # Open the template for the operation
-            template = open(str(self.resource_folder + self.settings[op]), 'r')
+        with open(str(self.resource_folder +self.settings["note-item-file"])) as note_file:
+            note_line = note_file.readline()
+        note_line = note_line.replace("<!--*NOTENAME*-->", note_name).replace("<!--*NOTEURL*-->", filename) + '\n'
 
-            # Split the line off of the marker
-            split_line = current_line.split(marker_s)
-            # If there is text before the delimiter, copy it to the new file before the template
-            # Then make it so there is only one line in the split_line list
-            if len(split_line) > 1:
-                temp_file.write(split_line[0])
-                split_line.remove(split_line[0])
-            # Copy the template in 
-            for temp_line in template:
-                temp_file.write(temp_line)
-            
-            # Write what came after the delimiter
-            if split_line:
-                temp_file.write(split_line[0])
-            
-            # Close the template file 
-            template.close()
+        old_index_file = open(self.settings["index-file"], "r")
+        tmp_index_filename = str(self.settings["index-file"] + '.tmp')
+        index_file = open(tmp_index_filename, "w")
 
-            # Check if the end marker is set for this operation
-            if marker_e:
-                # Prime the loop
-                end_marker_found = False
-                while not end_marker_found:
-                    # Read the file until the end marker is found
-                    old_line = old_file.readline()
-                    if marker_e in old_line:
-                        end_marker_found = True
+        start_of_edit_found = False
+        while not start_of_edit_found:
+            line = old_index_file.readline()
+            if "<!--*STARTEDIT*-->" in line:
+                start_of_edit_found = True
+            index_file.write(line)
 
-            # Remove the marker and operation since we have completed them
-            del markers_to_find[op]
-            operations_to_do.remove(op)
+        if note_category:
+            category_found = False
+            while not category_found:
+                line = old_index_file.readline()
+                if "<!--*ENDEDIT*-->" in line:
+                    index_file.write(category_line)
+                    index_file.write(note_line)
+                    index_file.write(line)
+                    category_found = True
+                elif "note-list-category" in line and note_category in line.split("</i>")[1]:
+                    index_file.write(line)
+                    index_file.write(note_line)
+                    category_found = True
+                else:
+                    index_file.write(line)
         else:
-            # Otherwise copy the old file over
-            temp_file.write(line)
+            index_file.write(note_line)
         
-    # Close the files
-    old_file.close()
-    temp_file.close()
+        for line in old_index_file:
+            index_file.write(line)
+        
+        index_file.close()
+        old_index_file.close()
 
-    # Overwrite the old file with the newly updated one
-    replace(temp_filename, filename)
+        replace(tmp_index_filename, self.settings["index-file"])
+
+    def process_file(self, filename):
+
+        # Open file and temp file to write to
+        old_file = open(filename, 'r')
+        temp_filename = str(filename+'.tmp')
+        temp_file = open(temp_filename, 'w')
+        
+        operations_to_do = self.operations.copy()
+        markers_to_find = self.markers.copy()
+        
+        temp_file.write("<!--*INDEXED*-->\n")
+
+        # Iterate through and parse the file
+        # Doing one operation at a time
+        num_line = 0
+        for line in old_file:
+            
+            current_line = line.strip()
+            if len(markers_to_find) > 0 and markers_to_find[operations_to_do[0]][0] in current_line:
+                # Set the current operation to the next to do
+                op = operations_to_do[0]
+                # Set the beginnning marker
+                marker_s = markers_to_find[operations_to_do[0]][0]
+                # Set the end marker
+                marker_e = markers_to_find[operations_to_do[0]][1]
+                # Open the template for the operation
+                template = open(str(self.resource_folder + self.settings[op]), 'r')
+
+                # Split the line off of the marker
+                split_line = current_line.split(marker_s)
+                # If there is text before the delimiter, copy it to the new file before the template
+                # Then make it so there is only one line in the split_line list
+                if len(split_line) > 1:
+                    temp_file.write(split_line[0])
+                    split_line.remove(split_line[0])
+                # Copy the template in 
+                for temp_line in template:
+                    temp_file.write(temp_line)
+                
+                # Write what came after the delimiter
+                if split_line:
+                    temp_file.write(split_line[0])
+                
+                # Close the template file 
+                template.close()
+
+                # Check if the end marker is set for this operation
+                if marker_e:
+                    # Prime the loop
+                    end_marker_found = False
+                    while not end_marker_found:
+                        # Read the file until the end marker is found
+                        old_line = old_file.readline()
+                        if marker_e in old_line:
+                            end_marker_found = True
+
+                # Remove the marker and operation since we have completed them
+                del markers_to_find[op]
+                operations_to_do.remove(op)
+            else:
+                # Otherwise copy the old file over
+                temp_file.write(line)
+            
+        # Close the files
+        old_file.close()
+        temp_file.close()
+
+        # Overwrite the old file with the newly updated one
+        replace(temp_filename, filename)
 
 
 
